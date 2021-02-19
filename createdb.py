bad5100196fe8f78ca840f61f20b20439b2c162a
abhishe53singh <62184031+abhishe53singh@users.noreply.github.com>
2021-02-19 16:18:08 +0530

    Add files via upload

---
+++ createdb.py
@@ -0,0 +1,26 @@
+#import mysql.connector
+#conn = mysql.connector.connect(
+#    host="localhost",
+#    user="root",
+#    passwd="1234",
+#    database="test",
+#    port=3306
+#)
+import sqlite3 as s
+
+conn=s.connect('bank.sqlite3')
+cursor = conn.cursor()
+cursor.execute(
+    "CREATE TABLE EMP_DETAILS(EID INT(10) PRIMARY KEY, NAME VARCHAR(20), PASSWORD VARCHAR(20), PHNO INT(10) UNIQUE)")
+cursor.execute(
+    "CREATE TABLE ADMIN_DETAILS(AID INT(10) PRIMARY KEY, NAME VARCHAR(20), PASSWORD VARCHAR(20), PHNO INT(10) UNIQUE)")
+cursor.execute(
+    "CREATE TABLE ITEM_DETAILS(IID INT(10) PRIMARY KEY, NAME VARCHAR(20), COST VARCHAR(20))")
+cursor.execute(
+    "CREATE TABLE BILLS(DATETIME VARCHAR(40), EID INT(20), AMOUNT DOUBLE, REFNO INT(10))")
+cursor.execute(
+    "CREATE TABLE DELETED_BILLS(DATETIME VARCHAR(40), EID INT(20), AMOUNT DOUBLE, REFNO INT(10))")
+sql = "INSERT INTO ADMIN_DETAILS VALUES (?,?,?,?)"
+val = (1, "admin", "admin", 1234567890)
+cursor.execute(sql, val)
+conn.commit()
